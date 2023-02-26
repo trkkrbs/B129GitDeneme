25dcb298ecabe7028fca1a2675eff5296a906bed
trkkrbs <74647469+trkkrbs@users.noreply.github.com>
2023-02-27 02:23:04 +0300

    Update Deneme.java

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -3,6 +3,7 @@ public class Deneme {
         System.out.println("Merhaba");
         System.out.println("Dunya");
         System.out.println("B129");
+        //Merhaba Dunya
     }
 }
 //Restore --> Tamamen Working Space' i temizler
