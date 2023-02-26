2fa00bd8492b04712b4a3ecd90b890f03cdd8718
trkkrbs <karabastarik@gmail.com>
2023-02-27 02:20:02 +0300

    v4

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -2,6 +2,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Dunya");
+        System.out.println("B129");
     }
 }
 //Restore --> Tamamen Working Space' i temizler
