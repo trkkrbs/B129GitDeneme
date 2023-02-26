fb6cb94eae09ea22dd06cb110e920e3307791735
trkkrbs <karabastarik@gmail.com>
2023-02-26 23:07:12 +0300

    v4

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -1,5 +1,8 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Dunya");
     }
 }
+//Restore --> Tamamen Working Space' i temizler
+//Stash --> Hafizaya alir
